660669639f406e292471eef6784322d96d007229
Melvin Chang <melvinchang21@gmail.com>
2023-08-15 16:24:22 -0700

    messages component

---
+++ src/components/Messages.tsx
@@ -0,0 +1,19 @@
+import "../styles/Messages.css"
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="messages">
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+
+    </div>
+  )
+}
+
+export default Messages
