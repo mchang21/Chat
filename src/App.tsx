6dd567559d114b7041ba285d3d62272d0e115640
Melvin Chang <melvinchang21@gmail.com>
2023-08-16 14:21:26 -0700

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    // <Register/>
-    // <Login/>
-    <Home/>
+
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/register" element ={<Register/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+        </Routes>
+    </BrowserRouter>
+
   )
 }
 
