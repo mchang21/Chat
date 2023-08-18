37fc4189320681fd46d7b13734634d833c7711b4
Melvin Chang <melvinchang21@gmail.com>
2023-08-18 14:51:14 -0700

    wrapped in chat context

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { AuthContextProvider } from './context/AuthContext.tsx';
+import { ChatContextProvider } from './context/ChatContext.tsx';
 
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthContextProvider>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>,
+        <ChatContextProvider>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>,
+        </ChatContextProvider>
     </AuthContextProvider>
 )
