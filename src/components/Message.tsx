996b4c7552fb5881fa87468bd18dc7757dfb07b5
Melvin Chang <melvinchang21@gmail.com>
2023-08-19 13:07:28 -0700

    adjust message div based on sender

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,15 +1,17 @@
-import { useContext } from "react";
-import "../styles/Message.css"
+import { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatAction, ChatContext, ChatState } from "../context/ChatContext";
+import { Timestamp } from "firebase/firestore";
+
+import "../styles/Message.css"
 
 interface MessageProps {
     message: {
-        // Define the properties of the message object
-        // For example:
-        text: string;
-        imageUrl: string;
-        timestamp: string;
+        date: Timestamp,
+        id: string,
+        img: string,
+        senderId: string,
+        text: string,
     };
 }
 
@@ -19,19 +21,28 @@ const Message: React.FC<MessageProps> = ({ message }) => {
     const { currentUser } = useContext(AuthContext);
     const { data } = useContext(ChatContext) as { data: ChatState; dispatch: React.Dispatch<ChatAction> };
 
+    const userPhotoURL = message.senderId === currentUser?.uid
+    ? currentUser?.photoURL
+    : data.user.photoURL;
+    
+    const ref = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        ref.current?.scrollIntoView({behavior: "smooth"});
+    }, [message]);
+
     return (
-        <div className="message owner">
-            {/* <div className="messageInfo">
-                <img
-                    src=""
-                    alt=""
-                />
-                <span>just now</span>
+        <div
+            ref={ref}
+            className={`message ${message.senderId === currentUser?.uid && "owner"}`}>
+            <div className="messageInfo">
+                {userPhotoURL && <img src={userPhotoURL} alt="" style={{}}/>}
+                <span>{ message.date.toDate().toLocaleTimeString() }</span>
             </div>
             <div className="messageContent">
-                <p>Hello</p>
-                <img src="https://nationaltoday.com/wp-content/uploads/2022/10/456841000-min.jpg" alt="" />
-            </div> */}
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
+            </div>
         </div>
     )
 }
